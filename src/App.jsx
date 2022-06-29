aa7c8ca2512e1761c4f55474e4c02c717133b16e
Lucas Silva <32777004+Lucas4994@users.noreply.github.com>
2022-06-29 17:01:29 -0300

    Add global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-function App() {
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import './global.css';
+
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+
+      <Post 
+        author="Lucas Silva" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolore reiciendis, repellendus fuga maiores voluptatem corrupti temporibus iste doloribus, hic ut a beatae. Cupiditate voluptatem quidem dolores, eos recusandae odit." 
+      />
+    </div>
   )
 }
 
-export default App
